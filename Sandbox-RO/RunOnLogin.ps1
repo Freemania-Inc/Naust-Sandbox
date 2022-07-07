87ceac34538e9354e3d2c0e76298b91aec34115e
John H. Freeman <john@freemania.pro>
2022-07-07 01:18:00 -0500

    squash to root of repository
    git ignore for windows sandbox
    
    updated sandbox configuration
    
    update for sandbox ro folder with config
    
    startup script renamed to use powershell instead of cmd
    
    sandbox loads basic developer environment for .NET
    
    Update RunOnLogin.ps1
    
    startup involves key and no components
    
    parameterized download paths
    
    squash to root of repository
    
    git ignore for windows sandbox
    
    updated sandbox configuration
    
    update for sandbox ro folder with config
    
    startup script renamed to use powershell instead of cmd
    
    sandbox loads basic developer environment for .NET
    
    java install with maven and checksum
    
    Create installMaven.ps1
    
    maven install script
    
    Create ACTInstall.ps1
    
    ACT program install is a separate script
    
    updated method of building save directory
    
    now uses built-in environment parameters and string concat

---
+++ Sandbox-RO/RunOnLogin.ps1
@@ -0,0 +1,11 @@
+$downloadSourceVSPro = "https://aka.ms/vs/17/release.ltsc.17.12/vs_professional.exe"
+$VSProSaveLocation = [System.String]::Concat($env:HOMEDRIVE, $env:HOMEPATH, "\Downloads\vs_BuildTools.exe")
+$VSProArgList = "-p --wait --productKey PM434-MN8GY-6CV9X-TJKRY-HVDF9"
+
+Write-Host "Downloading microsoft vs tools for compilation"
+Start-BitsTransfer -Source $downloadSourceVSPro -Destination $VSProSaveLocation -TransferType Download
+
+Write-Host "Installing MS Build tools Silently"
+Start-Process $VSProSaveLocation -ArgumentList $VSProArgList
+
+Write-Host "Done with Windows Sandbox startup..."
